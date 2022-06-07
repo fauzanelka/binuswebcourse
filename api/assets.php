19483c8d23005c39d14da2807b00201fde69d554
Fauzan Elka <hi@fauzanelka.id>
2022-06-07 19:44:34 +0700

    fix: add 0 return code

--- api/assets.php
+++ api/assets.php
@@ -8,19 +8,19 @@
 if ($_GET['file'] === 'robots.txt') {
     header('Content-type: text/plain');
     echo require __DIR__ . '/robots.txt';
-    exit;
+    exit(0);
 } else if ($_GET['file'] === 'favicon.ico') {
     header('Content-type: image/x-icon');
     echo require __DIR__ . '/favicon.ico';
-    exit;
+    exit(0);
 }
 
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
     echo require __DIR__ . '/assets/css/' . basename($_GET['file']);
-    exit;
+    exit(0);
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
     echo require __DIR__ . '/assets/js/' . basename($_GET['file']);
-    exit;
+    exit(0);
 }

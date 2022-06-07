56b858743e954a1feab1c82ab6f824ee8fe7163e
Fauzan Elka <hi@fauzanelka.id>
2022-06-07 19:49:11 +0700

    fix: change echo to readfile

--- api/assets.php
+++ api/assets.php
@@ -7,20 +7,20 @@
 
 if ($_GET['file'] === 'robots.txt') {
     header('Content-type: text/plain');
-    echo require __DIR__ . '/robots.txt';
+    readfile(__DIR__ . '/robots.txt');
     exit(0);
 } else if ($_GET['file'] === 'favicon.ico') {
     header('Content-type: image/x-icon');
-    echo require __DIR__ . '/favicon.ico';
+    readfile(__DIR__ . '/favicon.ico');
     exit(0);
 }
 
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/assets/css/' . basename($_GET['file']);
+    readfile(__DIR__ . '/assets/css/' . basename($_GET['file']));
     exit(0);
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/assets/js/' . basename($_GET['file']);
+    readfile(__DIR__ . '/assets/js/' . basename($_GET['file']));
     exit(0);
 }

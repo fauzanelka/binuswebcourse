e3d19d52e01d8b1e4bba05eb472eb39f19335ead
Fauzan Elka <hi@fauzanelka.id>
2022-06-07 19:39:32 +0700

    fix: add exit after return

--- api/assets.php
+++ api/assets.php
@@ -8,15 +8,19 @@
 if ($_GET['file'] === 'robots.txt') {
     header('Content-type: text/plain');
     echo require __DIR__ . '/robots.txt';
+    exit;
 } else if ($_GET['file'] === 'favicon.ico') {
     header('Content-type: image/x-icon');
     echo require __DIR__ . '/favicon.ico';
+    exit;
 }
 
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
     echo require __DIR__ . '/assets/css/' . basename($_GET['file']);
+    exit;
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
     echo require __DIR__ . '/assets/js/' . basename($_GET['file']);
+    exit;
 }

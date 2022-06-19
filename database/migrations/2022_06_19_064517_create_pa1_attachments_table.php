a90bb33889b456318d84e287ad831e8db2ed5f32
Fauzan Elka <hi@fauzanelka.id>
2022-06-19 16:06:25 +0700

    feat(personal): add database migration

---
+++ database/migrations/2022_06_19_064517_create_pa1_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pa1_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('pa1_id');
+            $table->foreign('pa1_id')->references('id')->on('pa1');
+            $table->string('name');
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pa1_attachments');
+    }
+};

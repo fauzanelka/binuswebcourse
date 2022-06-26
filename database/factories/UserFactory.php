674b66c0f0761fa749493a372f58382e2d71ae67
Fauzan Elka <hi@fauzanelka.id>
2022-06-26 18:54:51 +0700

    feat: add seeders and factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $genderList = ['male', 'female'];
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'gender' => $genderList[array_rand($genderList)],
+            'placeOfBirth' => $this->faker->city(),
+            'dateOfBirth' => $this->faker->dateTimeBetween('-50 years', '-20 years'),
         ];
     }
 

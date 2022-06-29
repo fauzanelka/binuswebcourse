4b49c87037901db5a9ef668d12151e8ee9aaa3e3
Fauzan Elka <hi@fauzanelka.id>
2022-06-29 20:17:28 +0700

    fix: wrong database path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('MIGRATE_ON_STARTUP', TRUE)) {
-            if (file_exists(base_path() . '/database/database.db')) {
-                copy(base_path() . '/database.db', '/tmp/database.db');
+            if (file_exists(base_path('database/database.sqlite'))) {
+                copy(base_path('database/database.sqlite'), '/tmp/database.db');
             }
         }
     }
